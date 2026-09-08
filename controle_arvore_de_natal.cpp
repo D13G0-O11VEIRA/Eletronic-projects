6e4f175ecbd6a70f2d4f014ca3f7553004f26c7e
D13G0 <diego.docr@gmail.com>
2026-09-08 10:39:14 -0300

    Initialize LED and button control for Christmas tree

--- controle_arvore_de_natal.cpp
+++ controle_arvore_de_natal.cpp
@@ -1,1 +1,50 @@
+// C++ code
 
+//PARAMETROS
+int pin_led1 = 2;
+int pin_led2 = 3;
+int pin_led3 = 4;
+int pin_led4 = 5;
+int pin_led5 = 6;
+int pin_led6 = 7;
+int pin_led7 = 8;
+int pin_led8 = 9;
+
+int pin_bt1 = 10;
+int pin_bt2 = 11;
+int pin_bt3 = 12;
+
+//VARIAVEIS
+int leitura_bt1 = 0; //Seq.
+int leitura_bt2 = 0; //Temp+
+int leitura_bt3 = 0; //Temp-
+
+int status_led = 0;
+
+//CONFIG. ENTRADAS ARDUINO
+void setup()
+{
+	pinMode(pin_led1, OUTPUT);
+  	pinMode(pin_led2, OUTPUT);
+	pinMode(pin_led3, OUTPUT);
+	pinMode(pin_led4, OUTPUT);
+	pinMode(pin_led5, OUTPUT);
+	pinMode(pin_led6, OUTPUT);
+	pinMode(pin_led7, OUTPUT);
+	pinMode(pin_led8, OUTPUT);
+	pinMode(pin_bt1, INPUT);
+  	pinMode(pin_bt2, INPUT);
+  	pinMode(pin_bt3, INPUT);
+}
+
+//LOOP DE ACIONAMENTO
+void loop()
+{
+	leitura_bt1 = digitalRead(pin_bt1); //VALOR DO BOTÃO
+	if (leitura_bt1 == 1)
+    {
+      	status_led = !status_led; // SELO DO BOTÃO
+    }  
+	
+    digitalWrite(pin_led1, status_led); // ACIONAMENTO DO LED
+}
