78cd6d109e875e5f2bb41c84fcd0b0920d7870ef
D13G0 <diego.docr@gmail.com>
2026-09-09 10:38:35 -0300

    Refactor button reading and sequence control logic

--- controle_arvore_de_natal.cpp
+++ controle_arvore_de_natal.cpp
@@ -43,11 +43,12 @@ void setup()
 // LOOP DE ACIONAMENTO
 void loop()
 {
-	leitura_bt1 = digitalRead(pin_bt1); //VALOR DO BOTÃO SEQ.
-  	leitura_bt2 = digitalRead(pin_bt2); //VALOR DO BOTÃO +
-   	leitura_bt3 = digitalRead(pin_bt3); //VALOR DO BOTÃO -
+	leitura_bt1 = digitalRead(pin_bt1);
+  	leitura_bt2 = digitalRead(pin_bt2);
+   	leitura_bt3 = digitalRead(pin_bt3);
 	
-	if (leitura_bt1 == 1) // INCREMENTO AO ACIONAR O BOTÃO
+	//BOTÃO SEQ.
+  	if (leitura_bt1 == 1) // INCREMENTO AO ACIONAR O BOTÃO
     {
     	seq_atual = seq_atual + 1;
       	if (seq_atual > 3) // RESET NA CONTAGEM
@@ -56,6 +57,58 @@ void loop()
     	}
   	}  
   	
+  	//BOTÃO +
+  	if (leitura_bt2 == 1) // INCREMENTO AO ACIONAR O BOTÃO
+    {
+    	tempo = tempo + 100;
+  	}  
+  
+  	//BOTÃO -
+  	if (leitura_bt3 == 1) // DECRESCIMO AO ACIONAR O BOTÃO
+    {
+    	tempo = tempo - 100;
+  	}  
+   
+  	// PADRÕES DAS SEQUENCIAS
+  	if (seq_atual == 1)
+    {
+    	digitalWrite(pin_led1, HIGH);
+        delay(tempo);
+        digitalWrite(pin_led2, HIGH);
+        delay(tempo);
+        digitalWrite(pin_led3, HIGH);
+        delay(tempo);
+        digitalWrite(pin_led4, HIGH);
+        delay(tempo);
+        digitalWrite(pin_led5, HIGH);
+        delay(tempo);
+        digitalWrite(pin_led5, HIGH);
+        delay(tempo);
+        digitalWrite(pin_led6, HIGH);
+        delay(tempo);
+        digitalWrite(pin_led7, HIGH);
+        delay(tempo);
+   		digitalWrite(pin_led8, HIGH);
+        delay(tempo);
+      
+   		digitalWrite(pin_led8, LOW);
+        delay(tempo);
+        digitalWrite(pin_led7, LOW);
+        delay(tempo);
+        digitalWrite(pin_led6, LOW);
+        delay(tempo);
+        digitalWrite(pin_led5, LOW);
+        delay(tempo);
+        digitalWrite(pin_led4, LOW);
+        delay(tempo);
+        digitalWrite(pin_led3, LOW);
+        delay(tempo);
+        digitalWrite(pin_led2, LOW);
+        delay(tempo);
+        digitalWrite(pin_led1, LOW);
+        delay(tempo);
+    }
+  
   	Serial.print("seq_atual = ");	
    	Serial.println(seq_atual);	
     Serial.print("tempo = ");	
