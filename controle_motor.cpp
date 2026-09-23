ebf37b074d699dee64ca54b1fcf4b92f791a0cf5
D13G0 <diego.docr@gmail.com>
2026-09-23 10:28:57 -0300

    Create controle_motor.cpp with initial setup
    Initialize variables and setup function for motor control.

---
+++ controle_motor.cpp
@@ -0,0 +1,25 @@
+// C++ code
+
+int pin_bt_h = 2;
+int pin_bt_a = 3;
+int pin_mt_h = 5;
+int pin_mt_a = 6;
+int pin_pt = A0;
+
+int lei_bt_h = 0;
+int lei_bt_a = 0;
+int lei_pt = 0;
+float ten = 0;
+int vel_mt_pwm = 0;
+float vel_mt_per = 0;
+int sen_gir = 0;
+
+void setup(){
+	pinMode()
+
+}
+
+void loop(){
+
+
+}
